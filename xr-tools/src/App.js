9020705ece99326bb92fd2b8ba85a30d4c29070f
directlink-opener <kgediya0898@gmail.com>
2023-09-18 17:48:58 +0530

    Updated the codebase with base react-app

---
+++ xr-tools/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Your XR Tools & Helpers All In One Place
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coming Soon
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
